9fb214ac71fe9108653d87e85f2cd5b8c89b9036
Ctrl+Ray <119338535+raytembo@users.noreply.github.com>
2024-11-13 13:40:01 +0200

    recreated the ui without any styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,45 +1,82 @@
-<div class="flex justify-center items-center bg-black">
-    <img 
-    src="./src/Images/technest.png" 
-    alt="technest logo"
-    class="w-60 h-60"
-    >
-    <div id="nav-links">
-        <a 
-        href=""
-        class="p-2 text-lg font-bold text-white"
-        >Home</a>
-        <a 
-        href=""
-        class="p-2 text-lg font-bold text-white"
-        >Cart</a>
-        <a 
-        href=""
-        class="p-2 text-lg font-bold text-white"
-        >Blog</a>
-        <a 
-        href=""
-        class="p-2 text-lg font-bold text-white"
-        >FAQ</a>
+<nav>
+  <img src="./src/Images/technest.png" alt="logo">
+  <div>
+    <a href="/">Home</a>
+    <a href="/Cart">Cart</a>
+    <a href="/Blog">Blog</a>
+    <a href="/profile">Profile</a>
+    <a href="/faq">FAQ</a>
+    <a href="/about">About Us</a>
+  </div>
+  <div>
+    <button>
+      Sign in
+    </button>
+
+    <button>
+      Sign Up
+    </button>
+
+  </div>
+</nav>
+
+<div>
+    <div>
+        <h1>
+          Welcome to TechNest
+        </h1>
+    
+        <h3>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis,
+          iure excepturi tempora commodi totam voluptas 
+          beatae exercitationem modi aut iusto cum nesciunt nisi 
+          dolore praesentium cumque vitae! Rerum, consequuntur animi!   
+        </h3>
     </div>
 
-    <div 
-        id="register"
-        class="pl-5"
-    >
-        <!-- svelte-ignore a11y_consider_explicit_label -->
-     <button
-         class="p-3 m-5 bg-blue-700 rounded-xl text-white"
-       >
-       Sign In
-     </button>
-
-     <button
-       class="p-3 bg-blue-700 rounded-xl text-white"
-     
-      >
-        Sign Up
-     </button>
+    <div>
+      <img src="" alt="hero">
     </div>
-
 </div>
+
+<h3>Today's Offers</h3>
+   
+<div>
+ 
+  <div>
+     <img src="" alt="product name">
+     <h3>Product Name</h3>
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+     <h4>$300</h4>
+  </div>
+</div>
+
+<div>
+    <div>
+      <img src="" alt="more detailed product">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
+          nemo nisi ullam natus vitae cumque quos
+          illum facilis quis cupiditate similique ea quisquam nostrum iusto, sint ab et sed voluptas!
+        </p>
+      <h2>$499 <em>Only</em></h2>
+    </div>
+</div>
+
+<h3>Our Product</h3>
+
+<div>
+    <div>
+      <img src="" alt="product name">
+      <h3>Product Name</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      <h4>$300</h4>
+    </div>
+    <button>
+      View More
+    </button>
+</div>
+
+<footer>
+  Tech nest 2024
+</footer>
