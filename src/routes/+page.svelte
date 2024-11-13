93070569eba3bd75b578788641a6af00970dabec
Ctrl+Ray <119338535+raytembo@users.noreply.github.com>
2024-11-13 09:21:52 +0200

    created a basic navigation bar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,45 @@
-<h1 class="text-green-400">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="flex justify-center items-center bg-black">
+    <img 
+    src="./src/Images/technest.png" 
+    alt="technest logo"
+    class="w-60 h-60"
+    >
+    <div id="nav-links">
+        <a 
+        href=""
+        class="p-2 text-lg font-bold text-white"
+        >Home</a>
+        <a 
+        href=""
+        class="p-2 text-lg font-bold text-white"
+        >Cart</a>
+        <a 
+        href=""
+        class="p-2 text-lg font-bold text-white"
+        >Blog</a>
+        <a 
+        href=""
+        class="p-2 text-lg font-bold text-white"
+        >FAQ</a>
+    </div>
+
+    <div 
+        id="register"
+        class="pl-5"
+    >
+        <!-- svelte-ignore a11y_consider_explicit_label -->
+     <button
+         class="p-3 m-5 bg-blue-700 rounded-xl text-white"
+       >
+       Sign In
+     </button>
+
+     <button
+       class="p-3 bg-blue-700 rounded-xl text-white"
+     
+      >
+        Sign Up
+     </button>
+    </div>
+
+</div>
